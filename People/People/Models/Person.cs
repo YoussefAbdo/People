bdb7101d73cd2eb8cd26fd3e388417dea9e6aab6
YoussefAbdo <mryoussefjoe@gmail.com>
2018-09-16 15:24:41 +0200

    Initial commit

---
+++ People/People/Models/Person.cs
@@ -0,0 +1,18 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace People.Models
+{
+    [Table("person")]
+    public class Person
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { set; get; }
+        [MaxLength(250), Unique]
+        public string Name { get; set; }
+    }
+}
